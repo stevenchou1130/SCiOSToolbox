8e578cd20b4c20664c1c7bc7fa50930409b86827
Steven C <steven.artemis@gmail.com>
2020-12-11 18:04:17 +0800

    First commit

---
+++ SCiOSToolbox/SCiOSToolbox.h
@@ -0,0 +1,18 @@
+//
+//  SCiOSToolbox.h
+//  SCiOSToolbox
+//
+//  Created by Steven on 2020/12/11.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SCiOSToolbox.
+FOUNDATION_EXPORT double SCiOSToolboxVersionNumber;
+
+//! Project version string for SCiOSToolbox.
+FOUNDATION_EXPORT const unsigned char SCiOSToolboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SCiOSToolbox/PublicHeader.h>
+
+
