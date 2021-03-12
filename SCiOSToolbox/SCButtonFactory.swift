8cd1b13797624f15382c6fcf50ab482434e3ec9a
Steven C <stevennote1130@gmail.com>
2021-03-12 10:39:09 +0800

    [Feat] Add simple button factory

---
+++ SCiOSToolbox/SCButtonFactory.swift
@@ -0,0 +1,38 @@
+//
+//  SCButtonFactory.swift
+//  SCiOSToolbox
+//
+//  Created by Steven on 2020/12/30.
+//
+
+import UIKit
+
+@objcMembers public class SCButtonFactory {
+    
+    // MARK: - Property
+
+    // MARK: - UI Content
+    
+    // MARK: - Initialization
+    init() {
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("\(Self.self) init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Public
+extension SCButtonFactory {
+ 
+    public func createButton() -> UIButton {
+        let b = UIButton(frame: .zero)
+        return b
+    }
+}
+
+// MARK: - Private
+extension SCButtonFactory {
+    
+}
